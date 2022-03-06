a35dffe4c48831e71ab076fba51f4c0e9be7d840
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-03-06 15:48:55 +0900

    basic setup for audio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::thread;
+
+
 fn main() {
     println!("Hello, world!");
+    let audiothread = thread::spawn(move || { return otopoiesis::audio::run()});
     otopoiesis::gui::run();
+    let audio_res = audiothread.join().unwrap();
 }
